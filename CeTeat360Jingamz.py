b046be0128d3687ac53c20ad61515da1cc541357
lijingfz <lijingfz@163.com>
2023-03-02 00:43:29 +0800

    添加输出到 Json 文件

--- CeTeat360Jingamz.py
+++ CeTeat360Jingamz.py
@@ -1,11 +1,14 @@
 # Jingamz@
 # 20230211
+# 20230228 --  调整数据集 -- 数据聚合正常
+# 20230301 --  生成Json 文件
 
 import datetime
 import boto3
 import pandas
 import numpy as np
 import json
+
 
 def condf(DimCol, UsageType):
     return pandas.DataFrame(
@@ -64,32 +67,34 @@ def GenCeReport(QueryOutput, merged_cost, dimkey1):
     return merged_cost
 
 
+MyStr2 = '''{{ "Keys": [ "{metric}","{region}"],"Metrics": {{"UnblendedCost": {{"Amount": "{Amount}","Unit": "USD"}} }} }}'''
+
 if __name__ == "__main__":
+
     # 指定AWS configure profile
     session = boto3.Session(profile_name='test')
     aws_client = session.client('ce')
-    start='2023-01-01'
-    end='2023-02-01'
+    # 指定查询范围，注意，[start,end)
+    start = '2023-01-01'
+    end = '2023-02-01'
+    # 构造DF
     merged_cost_service = condf('Services', 'UsageType')
+    # 以 services 和 UsageType 为维度出数据。
     response_service = getcedetail(aws_client, start, end, 'SERVICE', 'USAGE_TYPE')
+    # 生成DF
     cost_service = GenCeReport(response_service, merged_cost_service, 'Services')
-    cost_service.to_csv('/Users/lijing/Desktop/cost_service.csv', index=True)
+
     merged_cost_region = condf('Region', 'UsageType')
     response_region = getcedetail(aws_client, start, end, 'REGION', 'USAGE_TYPE')
     cost_region_filter = GenCeReport(response_region, merged_cost_region, 'Region')
+    # 360 filter NoRegion
     cost_region = cost_region_filter.query("Region != 'NoRegion'")
-    cost_region.to_csv('/Users/lijing/Desktop/cost_region.csv', index=True)
 
+    # 以 UsageType 为轴进行合并
     MyRegion = pandas.merge(cost_service, cost_region.drop([start], axis=1), on=['UsageType'],
-                             how='left')
+                            how='left')
 
-    # MyRegion = pandas.merge(cost_service, cost_region, on=['UsageType'],
-    #                         how='left')
-    # print(len(MyRegion))
-    # print(len(cost_service))
-    # print(len(cost_region))
-    print(MyRegion)
-
+    # 合并生成DataTransfer 类型的数据
     MyRegion['Services'] = np.where((MyRegion['UsageType'].str.contains('DataTransfer-Out-Bytes')) |
                                     (MyRegion['UsageType'].str.contains('DataTransfer-In-Bytes')) |
                                     (MyRegion['UsageType'].str.contains('AWS-In-Bytes')) |
@@ -100,21 +105,44 @@ if __name__ == "__main__":
                                     (MyRegion['UsageType'].str.contains('CloudFront-Out-Bytes')) |
                                     (MyRegion['UsageType'].str.contains('DataTransfer-Regional-Bytes'))
                                     , 'DataTransfer', MyRegion['Services'])
-    MyRegion['Services'] = np.where((MyRegion['Services'].str.contains('EC2 - Other')), 'Amazon Elastic Compute Cloud - Compute', MyRegion['Services'])
-    MyRegion.to_csv('/Users/lijing/Desktop/add-region.csv', index=True)
-    df = pandas.read_csv('/Users/lijing/Desktop/add-region.csv')
+    # 合并 'EC2 - Other' 到 'Amazon Elastic Compute Cloud - Compute'
+    MyRegion['Services'] = np.where((MyRegion['Services'].str.contains('EC2 - Other')),
+                                    'Amazon Elastic Compute Cloud - Compute', MyRegion['Services'])
+    MyRegion.to_csv('/tmp/add-region.csv', index=True)
+    df = pandas.read_csv('/tmp/add-region.csv')
     df.head()
-    bill_pivot = pandas.pivot_table(df, index=['Services','Region'], values=[start], aggfunc=np.sum, margins=True)
-    # bill_pivot = pandas.pivot_table(df, index=['Services', 'Region'], values=[start], aggfunc=np.sum)
-    # bill_pivot.sort_values(by=start, ascending=False, inplace=True)
-    print(bill_pivot)
-    bill_pivot.to_csv('/Users/lijing/Desktop/result.csv')
-    df1 = pandas.read_csv('/Users/lijing/Desktop/result.csv')
+    bill_pivot = pandas.pivot_table(df, index=['Services', 'Region'], values=[start], aggfunc=np.sum, margins=True)
+
+    bill_pivot.to_csv('/tmp/result.csv')
+    df1 = pandas.read_csv('/tmp/result.csv')
     usage = df1.rename(columns={start: 'Usage'})
-    # usage_list = usage.to_json(orient='split')
-    usage_list = usage.to_json(orient='records')
-    parsed = json.loads(usage_list)
-    print(json.dumps(parsed, indent=4))
-    jsonFile = open('/Users/lijing/Desktop/CE20230228.json', 'w')
-    jsonFile.write(json.dumps(parsed, indent=4))
+    list1 = []
+
+    for row in usage.itertuples():
+        dict_row = {
+            "Keys": [
+                row.Services,
+                row.Region
+            ],
+            "Metrics": {
+                "UnblendedCost": {
+                    "Amount": row.Usage,
+                    "Unit": "USD"
+                }
+            }
+        }
+        list1.append(dict_row)
+
+    dict_result = {
+        "TimePeriod": {
+            "Start": start,
+            "End": end
+        },
+        "Total": {},
+        "Groups": list1
+    }
+
+    # 注意这里需要替换放置输出的文件。
+    jsonFile = open('/Users/lijing/Desktop/CE20230301.json', 'w')
+    jsonFile.write(json.dumps(dict_result, indent=4))
     jsonFile.close()
